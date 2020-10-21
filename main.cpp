6f268115b760028e9801d2faafa61a8165622e6d
Babbaj <babbaj45@gmail.com>
2020-10-20 21:22:14 -0400

    write dimension tuple to file

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@
 #include <optional>
 #include <chrono>
 #include <ranges>
+#include <memory_resource>
+#include <vector>
 
+#define PQXX_HAVE_CONCEPTS
 #include <pqxx/pqxx>
 #include <args.hxx>
 
@@ -77,7 +80,9 @@ using i16 = int16_t;
 using i32 = int32_t;
 using i64 = int64_t;
 
-struct placeholder {};
+struct placeholder {
+    placeholder(const auto&) {}
+};
 
 using Hits =           std::tuple<i64, i64, i32, i32, i32, i32, i32, bool, i32>;
 using Blocks =         std::tuple<i32, i16, i32, i32, i64, i16, i16>;
@@ -96,18 +101,76 @@ enum class UpdateType : char {
     Delete
 };
 
+template<typename>
+constexpr bool is_tuple = false;
+template<typename... Ts>
+constexpr bool is_tuple<std::tuple<Ts...>> = true;
+
+template<typename Tuple> requires is_tuple<Tuple>
+Tuple rowToTuple(const pqxx::row& row) {
+    if (row.size() != std::tuple_size_v<Tuple>) {
+        throw std::runtime_error("row size wrong!!");
+    }
+
+    return [&]<size_t... I>(std::index_sequence<I...>) {
+        return std::make_tuple(row.at(I).get<std::tuple_element_t<I, Tuple>>().value()...);
+    }(std::make_index_sequence<std::tuple_size_v<Tuple>>{});
+}
+
+
+template<typename T>
+T* allocateElements(std::pmr::vector<T>& vec, size_t size) {
+    vec.resize(vec.size() + size);
+    return &vec.back() - (size - 1);
+}
+
+void serialize(std::pmr::vector<char>& vec, std::integral auto x) {
+    auto* buf = allocateElements(vec, sizeof(x));
+    memcpy(buf, &x, sizeof(x));
+}
+
+void serialize(std::pmr::vector<char>& vec, std::string_view str) {
+    const auto bufSize = str.size() + sizeof(int32_t);
+    auto* buf = allocateElements(vec, bufSize);
+    const int32_t len = str.size();
+    memcpy(buf, &len, sizeof(int32_t));
+    memcpy(buf + sizeof(int32_t), &str[0], len);
+}
+
+
+template<typename Tuple> requires is_tuple<Tuple>
+void serializeTupleToBuffer(const Tuple& tuple, std::pmr::vector<char>& vec) {
+    [&]<size_t... I>(std::index_sequence<I...>) {
+        (serialize(vec, std::get<I>(tuple)), ...);
+    }(std::make_index_sequence<std::tuple_size_v<Tuple>>{});
+}
+
+void outputTable(const fs::path& file, const pqxx::result& result) {
+    std::cout << "Outputting table to " << file.string() << '\n';
+    std::ofstream out(file);
+
+    for (const pqxx::row& row : result) {
+        const auto dim = rowToTuple<Dimensions>(row);
+        char allocator_buffer[1000000]; // should be more than enough for any row
+        std::pmr::monotonic_buffer_resource resource(allocator_buffer, sizeof(allocator_buffer));
+        std::pmr::vector<char> buffer{&resource};
+        buffer.reserve(sizeof(allocator_buffer) - 100); // probably unnecessary to under reserve
+
+        serializeTupleToBuffer(dim, buffer);
+        std::cout << "writing " << buffer.size() << " bytes to file\n";
+        out.write(&buffer[0], buffer.size());
+
+        std::cout << "Ordinal = " << std::get<0>(dim) << '\n';
+        std::cout << "Name = " << std::get<1>(dim) << '\n';
+    }
+}
+
 std::string selectNewestQuery(const std::string& table, const Incremental& inc, const std::string& oldValue) {
     return "SELECT * FROM "s + table + " WHERE "s + inc.column + " > "s + oldValue;
 }
 
 std::string selectAllQuery(const std::string& table) {
     return "SELECT * FROM "s + table;
-}
-
-void outputTable(const fs::path& file, const pqxx::result& result) {
-    std::cout << "Outputting table to " << file.string() << '\n';
-    std::ofstream out(file);
-    out << "This table has " << result.size() << " rows uwu\n";
 }
 
 void runBackup(pqxx::connection& db, const fs::path& rootOutput, const Table& table) {
